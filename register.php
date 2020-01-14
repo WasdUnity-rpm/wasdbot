f0a163dcc73348349132bf918e9f1d7ab4898372
WasdUnity-rpm <59863865+WasdUnity-rpm@users.noreply.github.com>
2020-01-14 09:23:11 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://wasdbot.herokuapp.com/execute.php';
+$BOT_TOKEN = '814130029:AAHzupDABDHbvUIsQJnpqbSiHYeTOAOIn40';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
